d7556904c4831ce2374635759b6525c8c375a764
Sigmund Howe <sig@howeville.com>
2024-04-14 04:22:31 +0800

    Implementing Database Backend Pt1
    + Added a basic database schema with four tables, User, Session, Request and Submission

--- server.py
+++ server.py
@@ -1,10 +1,17 @@
 import flask
+from models import db, User, Session, Request, Submission, Ranking
 
 app = flask.Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
 
 @app.route('/')
 def index():
     return flask.render_template('index.html')
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()  # Create tables in database if they don't exist
     app.run()
